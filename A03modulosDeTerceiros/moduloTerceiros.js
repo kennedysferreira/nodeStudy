6d156212628491697c5fbe781dc8c2d2e0882bb7
kennedysferreira <sferreira.kennedy@gmail.com>
2024-05-07 22:58:09 +0100

    Update moduloTerceiros.js

--- A03modulosDeTerceiros/moduloTerceiros.js
+++ A03modulosDeTerceiros/moduloTerceiros.js
@@ -2,5 +2,5 @@
 e colocar o codigo 'npm i 'biblioteca''*/
 //Apasta lodash foi instalada a partir do 'npm'
 
-const _ = require('lodash')// buscando um arquivo a partir de uma biblioteca de terceiro
-setInterval(() => console.log(_.random(1, 1000)), 2000)
+const _ = require("lodash"); // buscando um arquivo a partir de uma biblioteca de terceiro
+setInterval(() => console.log(_.random(1, 1000)), 2000);
